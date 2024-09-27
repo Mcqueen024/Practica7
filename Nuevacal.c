0738941cba7295db462856ad620acfa7632656c1
Mcqueen024 <barcenaspalaciosisrael@gmail.com>
2024-09-27 14:53:10 -0600

    Create Nuevacal.c

---
+++ Nuevacal.c
@@ -0,0 +1,56 @@
+/*Barcenas Palacios Israel
+ejercicio 4 Calculadora con funciones
+Práctica 7 27/09/2027*/
+
+#include <studio.h>
+#include <math.h>
+int resp;
+
+int suma(int a, int b)
+{
+  resp= a+b;
+return(resp);
+}
+  int resta(int a,int b)
+{
+  resp= a-b;
+    return(resp);
+}
+  int mult(int a, int b)
+  resp=a*b;
+}
+int div(int a, int b)
+{
+resp=a/b;
+}
+int exp(int a,int b)
+{
+  resp=pow(a,b);
+}
+
+int main[]
+{
+int a=0,b=0;
+  int respuestas;
+
+  printf("dime que operación deseas realizar:\n");
+  printf("1.- suma\n");
+    printf("2.- resta\n");
+    printf("3.- multiplicación\n");
+    printf("4.- división\n");
+    printf("5.- exponente\n");
+  scanf("%d", &option);
+
+switch(option)
+  {
+    
+
+
+
+
+
+
+
+
+  
+}
